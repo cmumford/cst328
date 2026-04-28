60073bd34e8528f0ea4d8c616645d1935eebd7f0
Chris Mumford <cmumford@cmumford.com>
2026-04-28 18:17:36 -0400

    Remove embassy-time dependency

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@ mod cst328;
 
 pub use cst328::{Error, TouchData, TouchPoint};
 
-// Conditional re-export so `Cst328` always exists for users
 #[cfg(feature = "use_sync")]
 pub use cst328::Cst328Sync as Cst328;
 
