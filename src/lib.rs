d35da21ec66d1f7081d412d0816da9a869621480
Chris Mumford <cmumford@cmumford.com>
2026-04-28 20:05:12 -0400

    Export Cst328Sync, Cst328Async, and Cst328 (alias)

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,13 @@ mod cst328;
 pub use cst328::{Error, TouchData, TouchPoint};
 
 #[cfg(feature = "use_sync")]
-pub use cst328::Cst328Sync as Cst328;
+pub use crate::cst328::Cst328Sync;
 
 #[cfg(feature = "use_async")]
-pub use cst328::Cst328Async as Cst328;
+pub use crate::cst328::Cst328Async;
+
+#[cfg(all(feature = "use_sync", not(feature = "use_async")))]
+pub use crate::cst328::Cst328Sync as Cst328;
+
+#[cfg(all(feature = "use_async", not(feature = "use_sync")))]
+pub use crate::cst328::Cst328Async as Cst328;
