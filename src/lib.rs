f102fcedb42da456e6a7dd40384ef83649a337a2
Chris Mumford <cmumford@cmumford.com>
2026-04-28 20:06:11 -0400

    Fail build if neither use_sync nor use_async are defined

--- src/lib.rs
+++ src/lib.rs
@@ -15,3 +15,14 @@ pub use crate::cst328::Cst328Sync as Cst328;
 
 #[cfg(all(feature = "use_async", not(feature = "use_sync")))]
 pub use crate::cst328::Cst328Async as Cst328;
+
+#[cfg(not(any(feature = "use_sync", feature = "use_async")))]
+compile_error!(
+    r#"You must enable at least one of the features `use_sync` or `use_async`.
+
+       Most users want the blocking version:
+           cst328 = { version = "0.x", features = ["use_sync"] }
+
+       For async (Embassy) use:
+           cst328 = { version = "0.x", features = ["use_async"] }"#
+);
