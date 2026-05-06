7a25dca0a7b7867dd2a8029ece6f629b6e611644
Chris Mumford <cmumford@cmumford.com>
2026-05-06 12:05:22 -0400

    Use hardcoded 5 for size of register grouping for single finger

--- src/cst328.rs
+++ src/cst328.rs
@@ -332,7 +332,7 @@ impl<I2C: I2cBound> Cst328<I2C> {
             return Err(Error::InvalidData);
         }
 
-        let mut response = [0u8; core::mem::size_of::<FingerEntry>()];
+        let mut response = [0u8; 5];
         let reg_addr: u16 = Register::Finger1 as u16 + Self::get_finger_offset(index);
         let addr = reg_addr.to_be_bytes();
         self.i2c
